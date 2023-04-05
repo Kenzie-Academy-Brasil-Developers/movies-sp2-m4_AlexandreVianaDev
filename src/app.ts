b91951e083c11479959290c673582f0810a3bc6c
AlexandreVianaDev <alexandreviana94@hotmail.com>
2023-04-05 15:23:04 -0300

    feat: crud

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import { startDatabase } from "./database";
+import {
+  deleteMovie,
+  insertMoviesFormat,
+  retrieveMovieById,
+  retrieveMovies,
+  updateMovie,
+} from "./logic";
+import { verifyIfIdExists, verifyIfNameExists } from "./middlewares";
 
 const app: Application = express();
 app.use(express.json());
 
-// app.post("/movies", insertQuery)
+app.post("/movies", verifyIfNameExists, insertMoviesFormat);
+app.get("/movies", retrieveMovies);
+app.get("/movies/:id", verifyIfIdExists, retrieveMovieById);
+app.patch("/movies/:id", verifyIfIdExists, verifyIfNameExists, updateMovie);
+app.delete("/movies/:id", deleteMovie);
 
 const PORT: number = 3000;
 const runningMsg = `Server is running on http://localhost:${PORT}`;
--- src/database.ts
+++ src/database.ts
@@ -8,23 +8,7 @@ export const client: Client = new Client({
   port: 5432,
 });
 
-client.connect();
-console.log("Conexão feita.");
-
-// export const queryString: string = `
-//     INSERT INTO movies
-//         (name, category, duration, price)
-//     VALUES
-//         ('Exterminador do Futuro', 'Ação',  5292, 29.90)
-//     RETURNING *;
-//   `;
-
-// client.query(queryString).finally(async () => {
-//   await client.end();
-//   console.log("Conexão encerrada.");
-// });
-
-export const startDatabase = async () => {
+export const startDatabase = async () : Promise<void> => {
   await client.connect();
   console.log("Database connected.");
 };
